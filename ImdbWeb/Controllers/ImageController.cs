89096a2272ae7922e03b53ba5ab039e27ef18ba3
aeinbu <arjan@einbu.no>
2015-08-18 14:26:56 +0200

    Supports Azure Blobs for covers

--- ImdbWeb/Controllers/ImageController.cs
+++ ImdbWeb/Controllers/ImageController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
@@ -10,31 +14,47 @@ namespace ImdbWeb.Controllers
 	public class ImageController : Controller
 	{
 		[Route("Image/{format}/{id}.jpg")]
-		public ActionResult CreateImage(string format, string id)
+		public async Task<ActionResult> CreateImage(string format, string id)
 		{
-			string relPath = $"~/App_Data/covers/{id}.jpg";
-			string absPath = Server.MapPath(relPath);
+			// koble til en storage-account
+			var connectionString = CloudConfigurationManager.GetSetting("msu2015_AzureStorageConnectionString");
+			var acct = CloudStorageAccount.Parse(connectionString);
 
-			if (!System.IO.File.Exists(absPath)) return HttpNotFound();
+			// åpne en konteiner
+			var client = acct.CreateCloudBlobClient();
 
-			var img = new WebImage(absPath);
+			var container = client.GetContainerReference("covers");
+			
+			// hente en fil
+			var blob = container.GetBlockBlobReference($"{id}.jpg");
 
-			switch (format.ToLower())
+			if (!await blob.ExistsAsync()) return HttpNotFound();
+
+			using (var ms = new MemoryStream())
 			{
-				case "thumb":
-					img.Resize(100, 1000)
-						.Write();
-					return new EmptyResult();
+				// laste ned som stream
+				await blob.DownloadToStreamAsync(ms);
 
-				case "medium":
-					img.Resize(300, 3000)
-						.AddTextWatermark("Ingars Movie Database")
-						.AddTextWatermark("Ingars Movie Database", "White", padding: 7)
-						.Write();
-					return new EmptyResult();
 
-				default:
-					return HttpNotFound();
+				var img = new WebImage(ms);
+
+				switch (format.ToLower())
+				{
+					case "thumb":
+						img.Resize(100, 1000)
+							.Write();
+						return new EmptyResult();
+
+					case "medium":
+						img.Resize(300, 3000)
+							.AddTextWatermark("Ingars Movie Database")
+							.AddTextWatermark("Ingars Movie Database", "White", padding: 7)
+							.Write();
+						return new EmptyResult();
+
+					default:
+						return HttpNotFound();
+				}
 			}
 		}
 	}
---
+++ ImdbWeb/Scripts/PersonDetails.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+
+	$('h3').on('click', function (ev) {
+
+		$(this).next().slideToggle('medium');
+
+	});
+
+});
